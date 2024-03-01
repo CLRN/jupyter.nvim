ce25d1589dd1cf08cf93317b6fe49736ba8213fa
clrn <clrnmail@gmail.com>
2024-03-01 15:55:20 +0000

    handle errors properly

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,20 @@
 #include "nvim.hpp"
-#include <fmt/core.h>
 
 auto run() -> boost::cobalt::task<int> {
     auto api = co_await nvim::Api::create("localhost", 6666);
-    try {
+    std::cout << co_await api.nvim_get_current_line() << std::endl;
 
-        std::cout << co_await api.nvim_get_current_line() << std::endl;
-
-        auto generator = api.nvim_create_autocmd({"BufEnter", "BufLeave"},
-                                                 {{"pattern", std::vector<nvim::Api::any>{"*.lua", "*.toml"}}});
-        while (generator) {
-            auto msg = co_await generator;
-            const auto buf = msg.as_vector().front().as_multimap();
-            std::cout << "new buf " << std::endl;
-            for (const auto& [k, v] : buf) {
-                std::cout << k.as_string() << "=" << (v.is_string() ? v.as_string() : std::to_string(v.as_uint64_t()))
-                          << std::endl;
-            }
+    auto generator = api.nvim_create_autocmd({"BufWritePost", "BufReadPost_"},
+                                             {{"pattern", std::vector<nvim::Api::any>{"*.lua", "*.toml"}}});
+    while (generator) {
+        auto msg = co_await generator;
+        const auto buf = msg.as_vector().front().as_multimap();
+        std::cout << "read/write" << std::endl;
+        for (const auto& [k, v] : buf) {
+            std::cout << k.as_string() << "=" << (v.is_string() ? v.as_string() : std::to_string(v.as_uint64_t()))
+                      << std::endl;
         }
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
     }
-
     //
     //
     //     co_await client.call("nvim_exec2", func, std::map<std::string, std::string>{});
@@ -47,4 +40,5 @@ int main(int argc, char* argv[]) {
     boost::cobalt::this_thread::set_executor(ctx.get_executor());
     auto f = boost::cobalt::spawn(ctx, run(), boost::asio::use_future);
     ctx.run();
+    return f.get();
 }
